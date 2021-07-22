98f4f8c2eaf9a4aeca006c08b0af81c3d0e5ed00
lyrahgames <lyrahgames@mailbox.org>
2021-07-22 13:13:20 +0200

    Tweak package structure

--- buildfile
+++ buildfile
@@ -1,2 +1,6 @@
-./: {*/ -build/} manifest legal{COPYING.md} doc{README.md AUTHORS.md}
+./: manifest legal{COPYING.md} doc{README.md AUTHORS.md}
+
+./: sources/
+
+./: tests/
 tests/: install = false
--- tests/version.cpp
+++ tests/version.cpp
@@ -1,5 +1,6 @@
 import<iomanip>;
 import<iostream>;
+import<sstream>;
 
 import lyrahgames.random;
 
@@ -7,24 +8,26 @@ using namespace std;
 using namespace lyrahgames::random;
 
 int main() {
-  cout << left << setw(25) << "version::number"
-       << " = " << right << setw(30) << version::number << '\n'
-       << left << setw(25) << "version::string"
-       << " = " << right << setw(30) << version::string << '\n'
-       << left << setw(25) << "version::id"
-       << " = " << right << setw(30) << version::id << '\n'
-       << left << setw(25) << "version::full"
-       << " = " << right << setw(30) << version::full << '\n'
-       << left << setw(25) << "version::major"
-       << " = " << right << setw(30) << version::major << '\n'
-       << left << setw(25) << "version::minor"
-       << " = " << right << setw(30) << version::minor << '\n'
-       << left << setw(25) << "version::patch"
-       << " = " << right << setw(30) << version::patch << '\n'
-       << left << setw(25) << "version::pre_release"
-       << " = " << right << setw(30) << version::pre_release << '\n'
-       << left << setw(25) << "version::snapshot_sn"
-       << " = " << right << setw(30) << version::snapshot_sn << '\n'
-       << left << setw(25) << "version::snapshot_id"
-       << " = " << right << setw(30) << version::snapshot_id << '\n';
+  stringstream stream;
+  stream << left << setw(20) << "version::number"
+         << " = " << right << setw(40) << version::number << '\n'
+         << left << setw(20) << "version::string"
+         << " = " << right << setw(40) << version::string << '\n'
+         << left << setw(20) << "version::id"
+         << " = " << right << setw(40) << version::id << '\n'
+         << left << setw(20) << "version::full"
+         << " = " << right << setw(40) << version::full << '\n'
+         << left << setw(20) << "version::major"
+         << " = " << right << setw(40) << version::major << '\n'
+         << left << setw(20) << "version::minor"
+         << " = " << right << setw(40) << version::minor << '\n'
+         << left << setw(20) << "version::patch"
+         << " = " << right << setw(40) << version::patch << '\n'
+         << left << setw(20) << "version::pre_release"
+         << " = " << right << setw(40) << version::pre_release << '\n'
+         << left << setw(20) << "version::snapshot_sn"
+         << " = " << right << setw(40) << version::snapshot_sn << '\n'
+         << left << setw(20) << "version::snapshot_id"
+         << " = " << right << setw(40) << version::snapshot_id << '\n';
+  cout << stream.str();
 }
